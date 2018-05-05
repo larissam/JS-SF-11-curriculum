52470f39347e596a47c1317de56031579cde8af4
Larissa Muramoto <lmuramoto@oath.com>
2018-05-04 18:55:06 -0700

    refine class 2 - data types and loops

---
+++ 02-data-types-loops/starter-code/data-types-codealong/app.js
@@ -0,0 +1,38 @@
+// ____________________________________________________________________________
+// PART 1 - Data types & typeof
+// ____________________________________________________________________________
+
+// Use the 'typeof' function to determine the types of the following elements:
+9
+"9"
+"nine"
+
+// your code here
+
+
+
+
+// ____________________________________________________________________________
+// PART 2 - Variables & console.log
+// ____________________________________________________________________________
+
+// Use 'console.log' and 'typeof' to log out the types of the following variables:
+let age = 30;
+let name = 'Larissa';
+let favoriteIceCreamFlavor = 'coffee';
+
+// your code here
+
+
+
+
+// Set the value of the totalPrice variable. 
+// The total price you pay is the base price plus sales tax (8.5% of the base price)
+// Use 'console.log' to log your result to the console
+let basePrice = 1000;
+let salesTaxRate = 0.085;
+let totalPrice;
+
+// your code here
+
+
