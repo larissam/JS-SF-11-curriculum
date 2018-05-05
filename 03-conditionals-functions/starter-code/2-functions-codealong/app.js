569bd54d967394932a780874c896eb4cd40530a2
Larissa Muramoto <lmuramoto@oath.com>
2018-05-05 10:51:19 -0700

    update functions codealong

--- 03-conditionals-functions/starter-code/2-functions-codealong/app.js
+++ 03-conditionals-functions/starter-code/2-functions-codealong/app.js
@@ -3,12 +3,47 @@
 //
 // Use a function declaration to make a function called catTalk, which logs 'Meow' to the console
 
+// Your code here!
+
+
+
+
+
+
 //
 // Part 2 - Function Expressions
 //
 // Use a function expression to make a function called catTalkExpression, which logs 'Meow' to the console
 
+// Your code here!
+
+
+
+
+
+
 //
 // Part 3 - Calling vs Declaring
 //
 // Call your functions to make sure they work!
+
+// Your code here!
+
+
+
+
+
+//
+// Part 4 - Return
+//
+
+// 1. Look at the code as-is. What do you expect the value of answer to be?
+// 2. Now uncomment out the 'return result;' line. What is the value of answer now?
+function sum(num1, num2) {
+    let result = num1 + num2;
+    console.log(result);
+    // return result;
+}
+
+let answer = sum(3, 4);
+console.log('answer: ', answer);
