23a63452f9b8d5a1b704a2db8b7a5f1f5a39c128
Larissa Muramoto <lmuramoto@oath.com>
2018-05-10 15:43:07 -0700

    cleaning up some events stuff

--- 08-events-jquery/starter-code/events-exercise/js/app.js
+++ 08-events-jquery/starter-code/events-exercise/js/app.js
@@ -13,9 +13,9 @@ let redBtn;
 let blueBtn;
 let yellowBtn;
 
-redBtn = document.querySelector('#r');
-blueBtn = document.querySelector('#b');
-yellowBtn = document.querySelector('#y');
+
+
+
 
 // STEP 2
 // After the variable declarations below, write statements setting the value of each variable to reference one of the three blocks in the middle of the page.
@@ -25,45 +25,27 @@ let redBlock;
 let blueBlock;
 let yellowBlock;
 
-redBlock = document.querySelector('.red');
-blueBlock = document.querySelector('.blue');
-yellowBlock = document.querySelector('.yellow');
+
+
 
 // STEP 3
 // Write a statement to add an event listener to the redBtn element. In response to the click event, it should add the class name 'hidden' to the redBlock element.
 
-redBtn.addEventListener('click', function() {
-    redBlock.className = "hidden";
-}, false);
+
+
 
 // STEP 4
 // Write a statement to add an event listener to the blueBtn element. In response to the click event, it should add the class name 'hidden' to the blueBlock element.
 
-blueBtn.addEventListener('click', function() {
-    blueBlock.className = "hidden";
-}, false);
+
 
 // STEP 5
 // Write a statement to add an event listener to the yellowBtn element. In response to the click event, it should add the class name 'hidden' to the yellowBlock element.
 
-yellowBtn.addEventListener('click', function() {
-    yellowBlock.className = "hidden";
-}, false);
 
-// STEP 6
-// Save your changes, open index.html in your browser, and test each of the buttons.
 
 
 // BONUS
 // Add an event listener for the "Show all blocks" button that removes the hidden class from all the colored block elements.
 // (HINT: Use a loop.)
 
-let showAll = document.querySelector('#show');
-let blocks = document.querySelectorAll('section');
-let colors = ['red','blue','yellow'];
-showAll.addEventListener('click', function() {
-    console.log("fired");
-    for(let i = 0; i < colors.length; i++) {
-        blocks[i].className = colors[i];
-    }
-}, false);
