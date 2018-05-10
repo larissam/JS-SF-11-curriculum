c9106242374f50f51f6e001b73a5ec9969bccb36
Larissa Muramoto <lmuramoto@oath.com>
2018-05-10 15:52:21 -0700

    moving around dom exercises

---
+++ 07-dom/starter-code/create-append-codealong/js/app.js
@@ -0,0 +1,12 @@
+// STEP 1 - Create a new li element
+// Add the 'list-group-item' class to it
+
+
+// STEP 2 - Create text content for it
+// The text content should equal 'Walk the dog'
+
+
+// STEP 3 - Add the text content as the li's child
+
+
+// STEP 4 - Add the content to the dom
