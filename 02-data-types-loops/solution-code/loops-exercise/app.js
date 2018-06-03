26dfe24a91143c925948cca11fc579044778fc8e
Larissa Muramoto <lmuramoto@palantir.com>
2018-06-03 15:11:38 -0700

    move homework; fix loops exercise

--- 02-data-types-loops/solution-code/loops-exercise/app.js
+++ 02-data-types-loops/solution-code/loops-exercise/app.js
@@ -1,50 +1,31 @@
-// Write code that creates a for loop that calculates 2 to the power of 10 and console.logs each step of the calculation. 
-// For instance, the for loop should console.log the result of 2^1, then the result of 2^2, etc., until it gets to 2^10.
+// Write code that logs the first 10 powers of 2 (ie. 2^1, 2^2, 2^3...) to the console
 
-// HINT (pseudocode):
-// Start by creating a variable to store the power of 2 you want to calculate. (In the above example, the value of this variable would be 10.)
-// Then create a for loop to calculate each step of this process and log it to the console.
+// BONUS 1: Rewrite your code to allow a user to enter the number of powers they want (instead of 10)
+// (Hint: Read up on the window.prompt method at https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt.)
 
-// IS THIS SUPPOSED TO BE A FUNCTION???
-// but we haven't learned functions yet
-// the directions aren't clear
-// would also be better to like, show what the console should log
-// i made it by default 2^10.
-// also, you can't define let twice... maybe I should call it like "bonus2Result"?
-// i totally did something different than the answer code. let's see if bunu can do this one.
-let result = 1;
-for(let i = 1; i <= 10; i++) {
-    result *= 2;
-    console.log(result);
+let numPowers = parseInt(window.prompt('Enter your exponent: '), 10);
+for(let i = 1; i <= numPowers; i++) {
+    console.log(2**i);
 }
 
-// BONUS 1: Rewrite your code to allow a user to enter the exponent value, rather than hard-coding it into your program. (Hint: Read up on the window.prompt method at https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt.)
+// BONUS 2: Rewrite your code to use a while loop rather than a for loop. 
+// (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while)
 
-let exponent = parseInt(window.prompt('Enter your exponent: '), 10);
-result = 1;
-for(let i = 1; i <= 10; i++) {
-    result *= exponent;
-    console.log(result);
+console.log('BONUS 2');
+let i = 1;
+while(i <= numPowers) {
+    console.log(2**i);
+
+    i += 1;
 }
 
-// BONUS 2: Rewrite your code to use a while loop rather than a for loop. (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while)
+// BONUS 3: Rewrite your code to use a do/while loop rather than a for loop or while loop. 
+// (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/do...while)
 
-let pow = 1;
-result = 1;
-while(pow <= 10) {
-    result *= 2;
-    console.log(result);
-
-    pow++;
-}
-
-// BONUS 3: Rewrite your code to use a do/while loop rather than a for loop or while loop. (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/do...while)
-
-pow = 1;
-result = 1;
+console.log('BONUS 3');
+i = 1;
 do {
-    result *= 2;
-    console.log(result);
+    console.log(2**i);
 
-    pow++;
-} while(pow <= 10);
+    i++;
+} while(i <= numPowers);
--- 02-data-types-loops/starter-code/loops-exercise/app.js
+++ 02-data-types-loops/starter-code/loops-exercise/app.js
@@ -1,35 +1,31 @@
 // Write code that logs the first 10 powers of 2 (ie. 2^1, 2^2, 2^3...) to the console
 
+
+
+
+
+
+
 // BONUS 1: Rewrite your code to allow a user to enter the number of powers they want (instead of 10)
 // (Hint: Read up on the window.prompt method at https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt.)
 
-let exponent = parseInt(window.prompt('Enter your exponent: '), 10);
-let result = 1;
-for(let i = 1; i <= 10; i++) {
-    result *= exponent;
-    console.log(result);
-}
 
 // BONUS 2: Rewrite your code to use a while loop rather than a for loop. 
 // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while)
 
-let pow = 1;
-result = 1;
-while(pow <= 10) {
-    result *= 2;
-    console.log(result);
+console.log('BONUS 2');
 
-    pow++;
-}
+
+
+
+
 
 // BONUS 3: Rewrite your code to use a do/while loop rather than a for loop or while loop. 
 // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/do...while)
 
-pow = 1;
-result = 1;
-do {
-    result *= 2;
-    console.log(result);
+console.log('BONUS 3');
 
-    pow++;
-} while(pow <= 10);
+
+
+
+
